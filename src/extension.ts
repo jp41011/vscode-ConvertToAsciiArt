f678564fbf1476b48122ce915690cc3463b15605
Juan <jp41011@gmail.com>
2019-06-11 23:26:39 -0700

    Started working on the showQuickPick feature. Trying things out, figuring things out. Need to clean up the code. Future feature: auto select text on line without auto selection. Have to go to sleep.

--- src/extension.ts
+++ src/extension.ts
@@ -6,13 +6,22 @@ import QuickPickItem = vscode.QuickPickItem;
 
 var figlet = require('figlet');
 
+function function1()
+{
+	console.log('function1');
+}
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-		console.log('Congratulations, your extension "converttoascii" is now active!');
+	console.log('Congratulations, your extension "converttoascii" is now active!');
+
+	function1();
+	function2();
+	console.log('testing1');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
@@ -22,60 +31,64 @@ export function activate(context: vscode.ExtensionContext) {
 
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World!');
+
+		console.log('Hello World function');
+		function1();
+		function2();
+		console.log('Hello World function end');
 	});
+
+	function function2()
+	{
+		console.log('function2');
+	}
 
 	//
 	let disposableConvertToAscii = vscode.commands.registerCommand('extension.convertToAscii', () => 
 	{
-		//var figlet = require('f	iglet');
+		console.log('convertToAscii Start');
+
+		var items: QuickPickItem[] = [];
+		
+		figlet.fontsSync().forEach(function (font: string) {
+			items.push({ label: font, description: "Use the " + font + " font" });
+		});
+
+		Window.showQuickPick(items).then(function (fontSelection) {
+			if (!fontSelection) {
+				return;
+			}
+			console.log('convertToAscii in showQuickPick');
+			//processSelection(e, d, sel, figlet.textSync, [selection.label]);
+			//fontSelection.label
+			funcConvertToAscii(fontSelection.label);
+		});
+		console.log('convertToAscii End');
+	});
+
+	function funcConvertToAscii(fontSelection: string)
+	{
+		console.log('funcConvertToAscii: ' + fontSelection);
 
 		// Get the active text editor
 		let editor = vscode.window.activeTextEditor;
 
 		if (editor) {
 			let document = editor.document;
-			let selectedText = editor.selection;
+			let selection = editor.selection;
 
 			// Get the word within the selection
-			let selectedWord = document.getText(selectedText);
+			let word = document.getText(selection);
+			
+			let asciiWord = figlet.textSync(word, {font: fontSelection});
 
-			var items: QuickPickItem[] = [];
-		
-			figlet.fontsSync().forEach(function (font: string) {
-				items.push({ label: font, description: "Use the " + font + " font" });
-			});
-
-			let asciiOutput = Window.showQuickPick(items).then(function (fontSelection) {
-				if (!fontSelection) {
-					return;
-				}
-
-				//processSelection(e, d, sel, figlet.textSync, [selection.label]);
-				
-				let userConfig = vscode.workspace.getConfiguration('convertToAscii');
-				//let favoriteFont = userConfig.get('favoriteFont');
-				let favoriteHorizontalLayout = userConfig.get('favoriteHorizontalLayout');
-				let favoriteVerticalLayout = userConfig.get('favoriteVerticalLayout');
-
-				let asciiWord = figlet.textSync(selectedWord, {font: fontSelection.label
-												, horizontalLayout: favoriteHorizontalLayout
-												, verticalLayout: favoriteVerticalLayout
-											});
-				console.log('ascii: ');
-				console.log(asciiWord);
-				return asciiWord;
-				//TODO here - not outputting to console log.
-				// maybe redo this whole thing
-				/*
-				editor.edit(editBuilder => {
-					editBuilder.replace(selectedText, asciiWord);
-				});
-				*/
+			editor.edit(editBuilder => {
+				editBuilder.replace(selection, asciiWord);
 			});
 		}
-	});
+	}
 
-	//
+	// Convert to ASCII using user's config (favorite) settings.
 	let disposableConvertToAscii_Favorite = vscode.commands.registerCommand('extension.convertToAscii_Favorite', () => 
 	{
 		//var figlet = require('figlet');
@@ -89,6 +102,8 @@ export function activate(context: vscode.ExtensionContext) {
 
 			// Get the word within the selection
 			let word = document.getText(selection);
+			
+			// Get user config settings
 			let userConfig = vscode.workspace.getConfiguration('convertToAscii');
 			let favoriteFont = userConfig.get('favoriteFont');
 			let favoriteHorizontalLayout = userConfig.get('favoriteHorizontalLayout');
