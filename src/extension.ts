779bbffd7b9a4b7acdbb07c2027d580eab23a238
Juan <jp41011@gmail.com>
2019-09-02 02:54:18 -0700

    Refactored. Cleaned up. ToDo: Remove commented code, add error handling for favorite font.

--- src/extension.ts
+++ src/extension.ts
@@ -6,48 +6,24 @@ import QuickPickItem = vscode.QuickPickItem;
 
 var figlet = require('figlet');
 
-function function1()
-{
-	console.log('function1');
-}
-
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "converttoascii" is now active!');
-
-	console.log('main ext func.');
-	function1();
-	function2();
-	console.log('testing1');
+	//console.log('Congratulations, your extension "converttoascii" is now active!');
+	//console.log('main func.');
+	//console.error('testing error');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-
-		console.log('Hello World function');
-		function1();
-		function2();
-		console.log('Hello World function end');
-	});
-
-	function function2()
-	{
-		console.log('function2');
-	}
 
 	// Select ASCII font and convert selected text into ASCII art.
 	let disposableConvertToAscii = vscode.commands.registerCommand('extension.convertToAscii', () => 
 	{
-		console.log('convertToAscii Start');
+		//console.log('convertToAscii Start');
 
 		var items: QuickPickItem[] = [];
 		
@@ -59,7 +35,7 @@ export function activate(context: vscode.ExtensionContext) {
 			if (!fontSelection) {
 				return;
 			}
-			console.log('convertToAscii in showQuickPick');
+			//console.log('convertToAscii in showQuickPick');
 			//processSelection(e, d, sel, figlet.textSync, [selection.label]);
 			//fontSelection.label
 			convertToAscii(fontSelection.label);
@@ -70,7 +46,7 @@ export function activate(context: vscode.ExtensionContext) {
 	// Convert to ASCII using user's config (favorite) settings.
 	let disposableConvertToAscii_Favorite = vscode.commands.registerCommand('extension.convertToAscii_Favorite', () => 
 	{
-		console.log('func_favorite()');
+		//console.log('func_favorite()');
 		//var figlet = require('figlet');
 
 		// Get user config settings
@@ -78,15 +54,18 @@ export function activate(context: vscode.ExtensionContext) {
 		let favoriteFont = userConfig.get('favoriteFont');
 		let temp2:string = String(favoriteFont);
 
+		// TODO: If favoriteFont is invalid show error
+		//Window.showErrorMessage('Testing Error Message');
+
 		convertToAscii(temp2);
 
-		console.log('func_favorite() end.');
+		//console.log('func_favorite() end.');
 	});
 
 	// Converts selected text into ASCII using the given fontSelection.
 	function convertToAscii(fontSelection: string|undefined)
 	{
-		console.log('convertToAscii: ' + fontSelection);
+		//console.log('convertToAscii: ' + fontSelection);
 
 		// Get the active text editor
 		let editor = vscode.window.activeTextEditor;
@@ -95,8 +74,8 @@ export function activate(context: vscode.ExtensionContext) {
 			let document = editor.document;
 			let selection = editor.selection;
 
-			// Get the word within the selection
-			let word = document.getText(selection);
+			// Get the selectedText within the selection
+			let selectedText = document.getText(selection);
 			
 			// Get user config settings
 			let userConfig = vscode.workspace.getConfiguration('convertToAscii');
@@ -104,7 +83,7 @@ export function activate(context: vscode.ExtensionContext) {
 			let favoriteHorizontalLayout = userConfig.get('favoriteHorizontalLayout');
 			let favoriteVerticalLayout = userConfig.get('favoriteVerticalLayout');
 
-			let asciiWord = figlet.textSync(word, {font: fontSelection
+			let asciiWord = figlet.textSync(selectedText, {font: fontSelection
 													, horizontalLayout: favoriteHorizontalLayout
 													, verticalLayout: favoriteVerticalLayout
 												});
@@ -115,7 +94,7 @@ export function activate(context: vscode.ExtensionContext) {
 		}
 	}
 
-	context.subscriptions.push(disposable);
+	//context.subscriptions.push(disposable);
 	context.subscriptions.push(disposableConvertToAscii);
 	context.subscriptions.push(disposableConvertToAscii_Favorite);
 }
